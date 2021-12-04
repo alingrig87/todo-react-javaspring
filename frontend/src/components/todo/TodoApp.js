9517575fad6359131b9e643ec63158fc7d468b41
Alin Grigorescu <alin.grig87@gmail.com>
2021-12-04 20:05:15 +0200

    Add Routing for Login and Welcome
    - use BrowserRouter in index.js
    - add Route elements inside Routes component in TodoApp

--- frontend/src/components/todo/TodoApp.js
+++ frontend/src/components/todo/TodoApp.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Login from '../auth/Login';
 import Welcome from '../Welcome';
+import { Routes, Route } from 'react-router-dom';
 
 function TodoApp() {
 	return (
 		<div>
-			<Login />
-			<Welcome />
+			<Routes>
+				<Route path="/welcome" element={<Welcome />} />
+				<Route path="/" element={<Login />} />
+			</Routes>
 		</div>
 	);
 }
